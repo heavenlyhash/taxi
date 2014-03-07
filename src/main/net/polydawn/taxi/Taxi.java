1aedc1bd75997ccd6677151fd0a3b7b336528731
Eric Myhre <hash@exultant.us>
2014-03-06 23:43:00 -0600

    demonstrate simplest possible tcp client/server pair dialing each other and playing pingpong on localhost.  anywhere this can go, you clearly have thread support, you clearly have network support, and you clearly have the ability to cat strings -- and thus are presumably fairly capable of doing some business in general.

--- src/main/net/polydawn/taxi/Taxi.java
+++ src/main/net/polydawn/taxi/Taxi.java
@@ -1,7 +1,12 @@
 package net.polydawn.taxi;
 
+import java.io.*;
+import net.polydawn.taxi.demo.*;
+
 public class Taxi {
-	public static void main(String... args) {
+	public static void main(String... args) throws IOException {
 		System.out.println("Hello, world.");
+		new SimplestServer().connect().serve();
+		System.out.println(new SimplestClient().connect().ping());
 	}
 }
---
+++ src/main/net/polydawn/taxi/demo/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Demonstrate taxi on $n$ platforms with robovm, flipping open the simplest possile
+ * barebones tcp socket and talking to itself, spamming threads wildly.
+ */
+package net.polydawn.taxi.demo;
